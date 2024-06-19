068b4e3c3580a7466fba757df978bd474861d424
Aswin P Krishnan <aswinpkrishnan94@gmail.com>
2024-06-19 13:24:13 +0100

    Create arduino_rx.ino

---
+++ UART/arduino_rx.ino
@@ -0,0 +1,22 @@
+void setup() {
+  Serial.begin(115200);
+  
+  // Define the LED pin as Output
+  pinMode (13, OUTPUT);
+  
+  Serial.println("Arduino UART Receiver");
+  Serial.println("-----------------------------");
+    
+}
+
+void loop() {
+
+  digitalWrite(13, LOW); 
+  //wait until something is received
+  while(! Serial.available());
+  digitalWrite(13, HIGH); 
+  //read the data
+  char bitread=Serial.read();
+  //print the data
+  Serial.print(bitread);
+}
