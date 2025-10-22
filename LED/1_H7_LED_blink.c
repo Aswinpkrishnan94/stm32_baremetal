240e4dedcf6a34dcc2a5636bb6f28c37f653c129
Aswin P Krishnan <aswinpkrishnan94@gmail.com>
2025-10-22 21:21:45 +0530

    Create 1_H7_LED_blink.c

---
+++ LED/1_H7_LED_blink.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "stm32h7xx.h"
+#include "stm32h7xx_hal.h"
+
+void delay(int ms)
+{
+	for(uint32_t i=0;i<ms;i++)
+	{
+		for(uint32_t j=0;j<2000;j++);
+	}
+}
+int main(void)
+{
+	volatile uint32_t *pclkctrlreg = (uint32_t*)0x580244E0;
+	volatile uint32_t *pgpiobmodereg = (uint32_t*)0x58020400;
+	volatile uint32_t *pgpiobodrreg = (uint32_t*)0x58020414;
+
+	*pclkctrlreg |= (1 << 1);
+
+	*pgpiobmodereg &= ~(3 << 0);
+	*pgpiobmodereg |= (1 << 0);
+
+	while(1)
+	{
+		*pgpiobodrreg |= (1 << 0);
+		delay(5000);
+		*pgpiobodrreg &= ~(1 << 0);
+		delay(5000);
+	}
+	return 0;
+}
